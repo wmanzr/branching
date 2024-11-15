5f44dc40c61d7dadefb7edfd9ed033b21c3ee099
master <master@mail.com>
2024-11-15 17:19:19 +0300

    update (2)

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ class Main{
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке main");
         System.out.print("первый");
+        System.out.print("второй");
     }
 }
